31761f1ce5ad0fe4269dd8c0c6d721b26c204c63
Tamule <122865779+Tamule@users.noreply.github.com>
2023-02-25 14:37:01 +0200

    Add files via upload

---
+++ Cities.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Cities() {
+  return (
+    <div className="Cities">
+      <Header />
+      <ul>
+        <li>Paris</li>
+        <li>Moscow</li>
+        <li>San Francisco</li>
+        <li>Sydney</li>
+      </ul>
+      <Footer />
+    </div>
+  );
+}
---
+++ Footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Footer() {
+  return <div className="Footer">Contact us to add more cities</div>;
+}
